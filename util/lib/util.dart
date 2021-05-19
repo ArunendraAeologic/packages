6af7d1a91cab5a6ef25aeefb0d98207cf4f7001b
arunendra <arunendra@aeologic.com>
2021-05-19 21:48:13 +0530

    update util package

---
+++ util/lib/util.dart
@@ -0,0 +1,18 @@
+library util;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class CommonUtil {
+  static void showToast({String message ="Empty message",Color bgColor = Colors.green,Color textColor = Colors.black, double fontSize = 16.0}){
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: bgColor,
+        textColor: textColor,
+        fontSize: fontSize
+    );
+  }
+}
